723d65ed54551ed746169c99c35a268c23e1b503
Muhammad-Hassan007 <hassanges11@gmail.com>
2024-07-31 15:19:21 +0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,5 @@
+document.getElementById('enter-site').addEventListener('click', function() {
+    document.getElementById('landing').style.display = 'none';
+    document.getElementById('main-content').style.display = 'block';
+    document.getElementById('main-content').style.animation = 'fadeIn 1s';
+});
